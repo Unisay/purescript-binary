e4c8cd8889cc99ed9869871649c57d15950d80a8
Yuriy Lazaryev <Yuriy.Lazaryev@zalando.de>
2017-10-23 13:24:56 +0200

    modAdd

--- src/Data/Binary/Nibble.purs
+++ src/Data/Binary/Nibble.purs
@@ -3,7 +3,7 @@ module Data.Binary.Nibble
   ) where
 
 import Conditional (ifelse)
-import Data.Binary.Class (class Binary, class FitsInt, class Fixed, Bit(..), Bits(..), _0, _1, add', addLeadingZeros, invert, toBinString, toBits)
+import Data.Binary.Class (class Binary, class FitsInt, class Fixed, Bit(Bit), Bits(Bits), _0, _1, add, add', addLeadingZeros, diffFixed, invert, toBinString, toBits, unsafeAdd)
 import Data.Binary.Overflow (Overflow(..))
 import Data.Maybe (Maybe(..))
 import Prelude hiding (add)
@@ -25,9 +25,7 @@ instance boundedNibble :: Bounded Nibble where
 instance binaryNibble :: Binary Nibble where
   _0 = Nibble _0 _0 _0 _0
   _1 = Nibble _0 _0 _0 _1
-
   invert (Nibble a b c d) = Nibble (invert a) (invert b) (invert c) (invert d)
-
   -- | Unsigned binary addition
   -- | Accepts a carry-over bit from the previous addition
   -- | Returns resulting `Nibble` with overflow bit
@@ -37,18 +35,18 @@ instance binaryNibble :: Binary Nibble where
         (Overflow b' b) = add' b0 b1 c'
         (Overflow a' a) = add' a0 a1 b'
     in Overflow a' (Nibble a b c d)
-
   toBits (Nibble a b c d) = Bits [a, b, c, d]
-
   leftShift z (Nibble a b c d) = Overflow a (Nibble b c d z)
   rightShift z (Nibble a b c d) = Overflow d (Nibble z a b c)
 
 instance fixedNibble :: Fixed Nibble where
   numBits _ = 4
-
   tryFromBits = addLeadingZeros 4 >>> f where
     f (Bits [a, b, c, d]) = Just (Nibble a b c d)
     f _ = Nothing
+  modAdd a b = f (add a b) where
+    f (Overflow (Bit false) x) = x
+    f (Overflow (Bit true) x) = unsafeAdd x _1
 
 instance fitsIntNibble :: FitsInt Nibble where
   toInt (Nibble a b c d) = 8 * bin a + 4 * bin b + 2 * bin c + bin d where
